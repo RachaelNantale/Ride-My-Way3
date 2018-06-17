171724e1c60fb02d4a70cc3c973128eb64e3db99
RachaelNantale <rachaelminani42@gmail.com>
2018-06-17 15:59:56 +0300

    Restructure Code

--- ridemyway/run.py
+++ ridemyway/run.py
@@ -1,11 +1,13 @@
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, Blueprint
 from flask_restful import Api, Resource, reqparse, fields, marshal
-from api import views
-from api.views import app
+import api
+from api.views import views
+from api import  views
+app = Flask(__name__, static_url_path="")
 
 
 
-
+app.register_blueprint(views)
 
 
 if __name__ == '__main__':
