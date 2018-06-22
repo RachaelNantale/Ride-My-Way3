4272ba000006b4d15362773fd68d3871a4685b96
RachaelNantale <rachaelminani42@gmail.com>
2018-06-22 15:21:03 +0300

    Add Base class for tests

---
+++ tests/BaseTest.py
@@ -0,0 +1,22 @@
+import unittest
+import json
+import pprint
+from app import create_app
+
+
+class BaseTest (unittest.TestCase):
+
+    def setUp(self):
+        self.app = create_app("testing")
+        self.client = self.app.test_client
+
+        self.rideoffer_body = {
+            "driver": "Amos Quito",
+            "pickup_point": "kanjokya",
+            "Destination": "Bukoto street",
+            "Time": "7:00pm",
+        }
+
+
+if __name__ == "__main__":
+    unittest.main()
