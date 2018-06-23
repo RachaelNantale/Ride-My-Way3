d7d8c1a4601f9618bfaa0561af59b0b3f5f79eb6
RachaelNantale <rachaelminani42@gmail.com>
2018-06-23 10:12:58 +0300

    Add Blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint
 from instance.config import app_config
-from .api.views import app_bp
+from .api.views.views import app_bp
+from .api.views.user_views import app_bps
 
 
 def create_app(config_module):
@@ -8,4 +9,6 @@ def create_app(config_module):
 
     app.config.from_object(app_config[config_module])
     app.register_blueprint(app_bp)
+    app.register_blueprint(app_bps)
+
     return app
