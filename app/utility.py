a21d695b4d46e1034eafe576717a059923152779
RachaelNantale <rachaelminani42@gmail.com>
2018-06-23 10:13:35 +0300

    Add Validation

--- app/utility.py
+++ app/utility.py
@@ -1,4 +1,6 @@
-from app.api.models import RideOffers
+import re
+from app.api.models.rideoffer import RideOffers
+from app.api.models.user import User
 
 
 class ValidateRideData:
@@ -16,11 +18,33 @@ class ValidateRideData:
             return {"message": "Please Fill in the Time"}
 
     def validate_no_symbols(self, driver, pickup_point, destination, time):
-        if driver.isalnum()is False or driver.strip(" ") == "  ":
+        if driver.isalnum()is False or driver.strip() == "":
             return {"message": "Please Fill in valid input"}
-        if pickup_point.isalnum()is False or pickup_point.strip(" ") == "":
+        if pickup_point.isalnum()is False or pickup_point.strip() == "":
             return {"message": "Please Fill in valid input"}
-        if destination.isalnum()is False or destination.strip(" ") == "":
+        if destination.isalnum()is False or destination.strip() == "":
             return {"message": "Please Fill in valid input"}
-        if time.isalnum()is False or time.strip(" ") == "":
+        if time.isalnum()is False or time.strip() == "":
             return {"message": "Please Fill in valid input"}
+
+
+class ValidateUserData:
+    """ Class to validate user authentication"""
+
+    def validate_user_login(self, username, password):
+        if username.strip() == "" or len(name.strip()) < 2:
+            return {"status": False, "message": "invalid, Enter name please"}
+
+        if not bool(re.fullmatch('^[A-Za-z ]*$', name)):
+            return {"status": False,
+                    "message": "Invalid characters not allowed"}
+
+        if not re.match(r"([\w\.-]+)@([\w\.-]+)(\.[\w\.]+$)", email):
+            return {"status": False, "message": "Enter valid email "}
+
+        if password.strip() == "":
+            return {"status": False, "message": "Enter password"}
+
+        if len(password) < 5:
+            return {"status": False, "message": "Password is too short, < 5"}
+        return {"status": True}
