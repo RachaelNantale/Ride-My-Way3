382393f955a59ac5bac5cf033e51fd9821c4c7ed
RachaelNantale <rachaelminani42@gmail.com>
2018-06-23 10:11:45 +0300

    Change File Structure

---
+++ app/api/models/rideoffer.py
@@ -0,0 +1,48 @@
+import uuid
+import json
+from flask import jsonify
+
+
+class RideOffers:
+    """
+    class for ride offers
+    """
+
+    def __init__(self, driver, pickup_point, destination, time, done):
+        self.id = uuid.uuid4().hex
+        self.driver = driver
+        self.pickup_point = pickup_point
+        self.destination = destination
+        self.time = time
+        self.done = done
+
+    def get_id(self):
+        return self.id
+
+    def get_driver(self):
+        return self.driver
+
+    def get_pickup_point(self):
+        return self.pickup_point
+
+    def get_destination(self):
+        return self.destination
+
+    def get_time(self):
+        return self.time
+
+    def get_done(self):
+        return self.done
+
+    def to_json(self):
+        """
+        json representation of the Ride model
+        """
+        return {
+            'id': self.id,
+            'Driver': self.driver,
+            'Pickup Point': self.pickup_point,
+            'Destination': self.destination,
+            'Time': self.time,
+            'done': self.done
+        }
---
+++ app/api/models/user.py
@@ -0,0 +1,99 @@
+import uuid
+import json
+import jwt
+from app.utility import RideOffers
+from datetime import datetime, timedelta
+from flask import jsonify
+
+
+class User:
+    """
+    Class to represent the User model
+    """
+
+    def __init__(self, username, email, password, phone):
+        self.id = uuid.uuid4().hex
+        self.username = username
+        self.email = email
+        self.password = password
+        self.phone = phone
+
+    def get_id(self):
+        return self.id
+
+    def get_username(self):
+        return self.username
+
+    def get_email(self):
+        return self.email
+
+    def get_password(self):
+        return self.password
+
+    def get_phone(self):
+        return self.phone
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'password': self.password,
+            'phone': self.phone
+
+        }
+
+    def use_token(parser):
+        parser.add_argument('token', location='headers')
+        args = parser.parse_args()
+        if not args['token']:
+            return {"status": False, "message": "Token is missing"}
+        decoded = decode_token(args['token'])
+        if decoded["status"] == "Failure":
+            return {"status": False, "message": decoded["message"]}
+        return {"status": True, "decoded": decoded}
+
+    def generate_token(id):
+        """Generates the access token to be used as the Authorization header"""
+
+        try:
+            # set up a payload with an expiration time
+            payload = {
+                'exp': datetime.utcnow() + timedelta(minutes=40),
+                # international atomic time
+                'iat': datetime.utcnow(),
+                # default  to user id
+                'sub': id
+            }
+            # create the byte string token using the payload and the SECRET key
+            jwt_string = jwt.encode(
+                payload,
+                current_app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            ).decode('UTF-8')
+            return jwt_string
+
+        except Exception as e:
+            # return an error in string format if an exception occurs
+            return str(e)
+
+    def decode_token(token):
+        """Decode the access token to get the payload and return
+        id """
+        try:
+            payload = jwt.decode(token, current_app.config.get('SECRET_KEY'))
+            return {
+                "id": payload['sub'],
+                "email": payload['email'],
+                "status": "Success"
+            }
+        except jwt.ExpiredSignatureError:
+            return {
+                "status": "Failure",
+                "message": "Expired token. Please log in to get a new token"
+            }
+        except jwt.InvalidTokenError:
+            return {
+                "status": "Failure",
+                "message": "Invalid token. Please register or login"
+            }
