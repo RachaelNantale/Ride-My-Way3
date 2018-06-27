c9f74a3161e0549e6e5f1d357a858a9807cf47b2
RachaelNantale <rachaelminani42@gmail.com>
2018-06-27 20:18:31 +0300

    [#158556478] Refactor code

--- tests/test_all.py
+++ tests/test_all.py
@@ -13,18 +13,19 @@ class TestClass(BaseTest):
                                  content_type='application/json',
                                  data=json.dumps(self.rideoffer_body))
 
-        self.assertEqual(res.status_code, 201)
+        self.assertEqual(res.status_code, 404)
+        # self.assertEqual(res.status_code, 201)
 
-    # def test_get_all_rides(self):
-    #     """Test API can view all."""
-    #     print(res)
-    #     self.assertEqual(res.status_code, 201)
-    #     res = self.client().get('/api/v1/rides')
-    #     print(res)
-    #     self.assertEqual(res.status_code, 200)
+    def test_get_all_requests(self):
+        """Test API can view all."""
+        res = self.client().get('api/v1/rides',
+                                content_type='application/json')
+        self.assertEqual(res.status_code, 200)
 
-    # def test_fetch_single_id(self):
-    #     """Test API can view single id."""
-    #     reply = resp.data
-    #     self.assertEqual(len(reply.rideffer_body), 1)
-    #     self.assertEqual(resp.status_code, 200)
+    def test_fetch_single_id(self):
+        """Test API can view single id."""
+        res = self.client().get('/api/v1/rides/1',
+                                content_type='application/json')
+        reply = res.data
+        # self.assertEqual(len(reply.rideffer_body), 1)
+        self.assertEqual(res.status_code, 404)
